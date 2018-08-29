5c2100214e363ccdccb452cfd49ad0d3256cf070
Jose Suarez <jose95sp@outlook.com>
2018-08-29 03:27:59 -0500

    just a bootstrap navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { ComponentsModule } from './shared/components/components.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    DashboardModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
