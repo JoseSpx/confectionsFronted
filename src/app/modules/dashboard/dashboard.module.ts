ad8f4301ba4b1d418a56f511518b54b711133220
JoseSpx <jose95sp@outlook.com>
2018-08-29 19:07:54 -0500

    added template shortcut dashboard

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -5,13 +5,15 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ShortcutsPaneComponent } from './shortcuts-pane/shortcuts-pane.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   imports: [
     CommonModule,
     MatInputModule,
     MatButtonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   declarations: [
     DashboardComponent,
