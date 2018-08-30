cc9caf96b048b8233174e8429e87fb3f76aa316a
JoseSpx <jose95sp@outlook.com>
2018-08-29 19:27:28 -0500

    added class shorcutPane

--- src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  private activities  = [] ;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  public getActivitiesData() : [] {
+
+  }
+
 }
+
+
+
+
--- src/app/modules/dashboard/shortcuts-pane/shortcuts-pane.component.ts
+++ src/app/modules/dashboard/shortcuts-pane/shortcuts-pane.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-shortcuts-pane',
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shortcuts-pane.component.css']
 })
 export class ShortcutsPaneComponent implements OnInit {
+
+  @Input()
+  public title : string;
+  @Input()
+  public imageUrl : string;
+  @Input()
+  public subtitle : string;
+  @Input()
+  public detailUrl : string;
 
   constructor() { }
 
