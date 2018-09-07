47ba1c4688830e8077e9ed1ea3d15eb172fa94ba
Jose Suarez <jose95sp@outlook.com>
2018-09-07 01:13:34 -0500

    changes server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static('./dist/confections-frontend'));
+app.use(express.static('./dist/confectionsFrontend'));
 app.get('/*', function(req, res) {
     res.sendFile("index.html", {"root": __dirname + "/dist/confectionsFrontend"});
 });
