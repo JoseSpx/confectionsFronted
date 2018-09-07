d09ab912b3cc5a21cfce36dbff274c3a02ecee4b
Jose Suarez <jose95sp@outlook.com>
2018-09-07 00:59:29 -0500

    chanes server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 const path = require('path');
 app.use(express.static('./dist/confections-frontend'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/confections-frontend/index.html'));
+    res.sendFile("index.html", {"root": __dirname + "/dist/confectionsFrontend"});
 });
 app.listen(process.env.PORT || 8080);
