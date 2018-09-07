dcc3cc4c048c92f1f4649d39d248504ad3135a66
Jose Suarez <jose95sp@outlook.com>
2018-09-06 20:18:42 -0500

    3 commit heroku

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -4,7 +4,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BoldPipe } from '../../shared/pipes/bold.pipe';
 import { ComponentsModule } from '../../shared/components/components.module';
 
 @NgModule({
@@ -16,8 +15,7 @@ import { ComponentsModule } from '../../shared/components/components.module';
     ComponentsModule
   ],
   declarations: [
-    DashboardComponent,
-    BoldPipe
+    DashboardComponent
   ],
   exports : [
     DashboardComponent
