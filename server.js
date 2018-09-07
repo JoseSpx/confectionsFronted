81fa3bc4ff495a135ec4c10548a70207f6e94008
Jose Suarez <jose95sp@outlook.com>
2018-09-07 00:15:58 -0500

    changes in server to just only dist

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //Install express server
 const express = require('express');
+const http = require('http')
 const path = require('path');
 
 const app = express();
@@ -9,8 +10,9 @@ app.use(express.static('./dist/confections-frontend'));
 
 app.get('/*', function(req,res) {
     
-    res.sendFile(path.join(__dirname+'/dist/confections-frontend/index.html'));
+    res.sendFile(path.join(__dirname));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const server = http.createServer(app)
+server.listen(port,() => console.log('Running'));
